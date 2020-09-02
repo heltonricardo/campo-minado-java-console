dc72ddbe110affdaa0991dc3e3bdc755333311c6
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-09-02 10:50:57 -0300

    Update Campo.java

--- src/io/github/heltonricardo/cm/modelo/Campo.java
+++ src/io/github/heltonricardo/cm/modelo/Campo.java
@@ -1,5 +1,22 @@
 package io.github.heltonricardo.cm.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Campo {
 
+	private final int linha;
+	private final int coluna;
+	
+	private boolean aberto;
+	private boolean minado;
+	private boolean marcado;
+	
+	List<Campo> vizinhos = new ArrayList<Campo>();
+	
+	Campo(int linha, int coluna) {
+		this.linha = linha;
+		this.coluna = coluna;
+	}
+	
 }
