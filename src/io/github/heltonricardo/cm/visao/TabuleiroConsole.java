bf53a759dc79ae19d876f3370ed660348a47719f
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-09-04 16:49:41 -0300

    Create TabuleiroConsole.java

---
+++ src/io/github/heltonricardo/cm/visao/TabuleiroConsole.java
@@ -0,0 +1,12 @@
+package io.github.heltonricardo.cm.visao;
+
+import io.github.heltonricardo.cm.modelo.Tabuleiro;
+
+public class TabuleiroConsole {
+
+	private Tabuleiro tabuleiro;
+	
+	public TabuleiroConsole(Tabuleiro tabuleiro) {
+		this.tabuleiro = tabuleiro;
+	}
+}
